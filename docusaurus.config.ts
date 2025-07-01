6d5206f34ccb67088e2ef7cd5924af2cdc88e4cd
อภิชาติ คำจีนสี <apichatkumjensi@pep.local>
2025-07-01 16:58:31 +0700

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://docs.compat-hub.com",
+  url: "https://COMPATTANA-RISE.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/jingseng-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
